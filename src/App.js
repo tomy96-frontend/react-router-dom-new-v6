e19a17b261d4b9e553ed7dcf0895040ade31bf7e
tomy96 <serestomy@gmail.com>
2021-12-02 23:55:15 +0100

    add hr in app js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   <>
    <div className='HeroText'>
       <h1>My first React Router Dom page V6 version</h1>
+      <hr />
       <nav>
         <Link to="/Home">Home</Link> 
         <Link to="/About">About</Link>
