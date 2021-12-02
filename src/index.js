acedbffac2a9f921f984482cf13732f8747f285f
tomy96 <serestomy@gmail.com>
2021-12-02 23:44:53 +0100

    add BrowserRouter import and add in render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
 
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+render(  
+<BrowserRouter>
+  <App />
+</BrowserRouter>,
+rootElement
+);
 
 
