951706eade1ba5276735d9aae3033cce5965e1af
tomy96 <serestomy@gmail.com>
2021-12-02 23:47:25 +0100

    add import Link and Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
   <>
    <div className='HeroText'>
       <h1>My first React Router Dom page V6 version</h1>
+      <nav>
+        <Link to="/Home">Home</Link> 
+        <Link to="/About">About</Link>
+      </nav>
     </div>
   </>
   );
