4605a16b437d09f99425049ba58c8717c4b66485
tomy96 <serestomy@gmail.com>
2021-12-02 23:40:16 +0100

    add const rootElement

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <App />
