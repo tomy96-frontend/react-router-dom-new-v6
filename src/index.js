04f6ee5852443396f83f2c682b4e8efdb9bb1f1c
tomy96 <serestomy@gmail.com>
2021-12-03 10:15:53 +0100

    add all import modul end route path

--- src/index.js
+++ src/index.js
@@ -7,20 +7,26 @@ import {
 } from "react-router-dom";
 import './index.css';
 import App from './App';
+import Home from './routes/Home'
+import About from './routes/About'
+import Features from './routes/Features'
+import Contact from './routes/Contact'
 
 
 
 const rootElement = document.getElementById("root");
-render(  
-<BrowserRouter>
- <Routes>
-   <Route path="/" element={<App />}>
-
-
-   </Route>
- </Routes>
-</BrowserRouter>,
-rootElement
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="features" element={<Features />} />
+        <Route path="Contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 
