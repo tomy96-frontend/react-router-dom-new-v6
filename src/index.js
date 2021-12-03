3af270395360bc51dad4dfc9b9043f424bc96526
tomy96 <serestomy@gmail.com>
2021-12-03 10:08:35 +0100

    add import new routes route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './index.css';
 import App from './App';
 
